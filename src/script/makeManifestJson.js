3917c74752bca2ff2418f32d0b1cda7c016bd5d6
Shinyaigeek <PhilisPaxil@gmail.com>
2019-12-22 17:50:23 +0900

    [fix]

--- src/script/makeManifestJson.js
+++ src/script/makeManifestJson.js
@@ -52,7 +52,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log("[Service Worker] Caching all: app shell and content");
-      return cache.addAll(appShellFiles);
+      return cache.addAll(appShellFiles).then(console.log).catch(console.log);
     })
   );
 });
